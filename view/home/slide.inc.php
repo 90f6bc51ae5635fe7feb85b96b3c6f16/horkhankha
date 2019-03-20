fc7c2bfcf152e7c0e545ed460742d6da57615298
boorapa <boorapa0147@gmail.com>
2019-03-20 09:51:16 +0700

    up

--- view/home/slide.inc.php
+++ view/home/slide.inc.php
@@ -84,74 +84,7 @@
         }
     </script>
 
-    <div class="box-search-home-index">
-        <div class="shadow-lg" style="background-color: white;">
-            <div class="row m-0 py-4">
-                <div class="col-sm-12 col-md-3 py-md-0 py-sm-3">
-                    <div class="text-left">
-                        <div class="">
-                            <select class="form-control  custom-select" id="location_id" name="location_id">
-                                <option value=""> Location</option>
-                                <?PHP for ($i = 0; $i < count($location); $i++) {  ?>
-                                <option value="<?PHP echo $location[$i]['location_id']; ?>" <?PHP if ($_GET['location_id'] == $location[$i]['location_id']) {
-                                                                                                echo 'selected';
-                                                                                            } ?>>
-                                    <?PHP if ($lan == 'TH') {
-                                        echo $location[$i]['location_name_th'];
-                                    } else {
-                                        echo $location[$i]['location_name_en'];
-                                    } ?>
-                                </option>
-                                <?PHP 
-                            } ?>
-                            </select>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-12 col-md-3 py-md-0 py-sm-3">
-                    <div class="text-left">
-                        <div class="">
-                            <select class="form-control  custom-select" id="product_types_id" name="product_types_id">
-                                <option value=""> Property type</option>
-                                <?PHP for ($i = 0; $i < count($type); $i++) {  ?>
-                                <option value="<?PHP echo $type[$i]['product_types_id']; ?>" <?PHP if ($_GET['product_types_id'] == $type[$i]['product_types_id']) {
-                                                                                                echo 'selected';
-                                                                                            } ?>>
-                                    <?PHP if ($lan == 'TH') {
-                                        echo $type[$i]['product_types_name_th'];
-                                    } else {
-                                        echo $type[$i]['product_types_name_en'];
-                                    } ?>
-                                </option>
-                                <?PHP 
-                            } ?>
-
-                            </select>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-3 py-md-0 py-sm-3 ">
-                    <div class="text-left">
-                        <div class="">
-                            <div class="">
-                                <input id="keyword" name="keyword" class="form-control" value="<?PHP echo $_GET['keyword']; ?>" placeholder="KEYWORD" />
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-3">
-                    <button onclick="search();" type="submit" class="btn btn-search">
-                        SEARCH
-                    </button>
-                </div>
-            </div>
-        </div>
-
-    </div>
+   
 
 </section>
 
